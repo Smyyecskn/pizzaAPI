71b683777b8fde79f4e8e76c46e7c2ee2865b85f
smyye <smyyeoztrk43@gmail.com>
2024-03-25 21:38:46 +0300

    user

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,44 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: (password) => passwordEncrypt(password),
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "users",
+  }
+);
+module.exports = new mongoose.model("User", userSchema);
