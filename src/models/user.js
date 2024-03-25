4cbb78a2b9bd36c123a97e3f161bb1df5592a794
smyye <smyyeoztrk43@gmail.com>
2024-03-25 22:37:36 +0300

    user pasword

--- src/models/user.js
+++ src/models/user.js
@@ -3,10 +3,12 @@
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 const { mongoose } = require("../configs/dbConnection");
-require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
+// User Model:
 
-const userSchema = new mongoose.Schema(
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
@@ -14,32 +16,63 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      // validate: [
+      //     (password) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password),
+      //     'Password type is not correct.'
+      // ],
+      // set: passwordEncrypt
+      //? Yöntem-1:
+      // set: (password) => {
+      //     if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)) {
+      //         return passwordEncrypt(password)
+      //     } else {
+      //         throw new Error('Password type is not correct.')
+      //     }
+      // },
+      //? Yöntem-2:
+      set: (password) => {
+        if (
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)
+        ) {
+          return passwordEncrypt(password);
+        } else {
+          return "wrong";
+        }
+      },
+      validate: (password) => password != "wrong",
+    },
+
     email: {
       type: String,
       trim: true,
       required: true,
       unique: true,
-      validate: (email) => email.includes("@") && email.includes("."), //validate create yaparken otomatik verir bu yuzden update de olsun dıye yazdık.
-    },
-    password: {
-      type: String,
-      trim: true,
-      required: true,
-      set: (password) => passwordEncrypt(password),
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Email type is not correct.",
+      ],
     },
 
     isActive: {
       type: Boolean,
       default: true,
     },
+
     isAdmin: {
       type: Boolean,
       default: false,
     },
   },
   {
-    timestamps: true,
     collection: "users",
+    timestamps: true,
   }
 );
-module.exports = new mongoose.model("User", userSchema);
+
+// Model:
+module.exports = mongoose.model("User", UserSchema);
