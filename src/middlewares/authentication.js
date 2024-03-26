de6ac0aacd356e767ee44595400f3d503d659c72
smyye <smyyeoztrk43@gmail.com>
2024-03-26 14:47:19 +0300

    update

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,4 +1,24 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+
+const Token = require("../models/token");
+
+module.exports = async (req, res, next) => {
+  const auth = req.headers?.authorization; // Token ...Tokenkey
+  const tokenKey = auth ? auth.split(" ") : null; //["Token" ""...Tokenkey"]
+
+  if (tokenKey) {
+    if (tokenKey[0] == "Token") {
+      const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+        "userId"
+      );
+      console.log(tokenData);
+      if (tokenData) {
+        req.user = tokenData.userId;
+      }
+    }
+  }
+  next();
+};
